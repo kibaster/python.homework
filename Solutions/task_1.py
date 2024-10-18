6127569563c0b7d26859a551949925096a2f5b08
kibaster <2006yanp@gmail.com>
2024-10-18 14:38:57 +0400

    1

--- Solutions/task_1.py
+++ Solutions/task_1.py
@@ -12,20 +12,16 @@ def pull(n:int):
         c5+=1
         if c4 == 10 and c5==90:
                 five_stars+=1
-                print('5*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
                 c5 =1
                 four_stars+=1
-                print('4*',i+1,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
                 c4=0
                 skip = True
                 continue
         if c5==90:
             five_stars+=1
-            print('5*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
             c5=0
         if c4==10:
             four_stars+=1
-            print('4*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
             c4=0
     return four_stars,five_stars
 print(pull(191))
