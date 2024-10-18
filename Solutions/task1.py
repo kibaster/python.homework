eb59df3c0185609d01ff38c10940591133468219
kibaster <2006yanp@gmail.com>
2024-10-18 13:58:44 +0400

    123

--- Solutions/task1.py
+++ Solutions/task1.py
@@ -1,1 +1,31 @@
-
+def pull(n:int):
+    four_stars = 0
+    five_stars = 0
+    c5 = 0
+    c4 = 0
+    skip = False
+    for i in range(1,n+1):
+        if skip:
+            skip = False
+            continue
+        c4+=1
+        c5+=1
+        if c4 == 10 and c5==90:
+                five_stars+=1
+                print('5*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
+                c5 =1
+                four_stars+=1
+                print('4*',i+1,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
+                c4=0
+                skip = True
+                continue
+        if c5==90:
+            five_stars+=1
+            print('5*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
+            c5=0
+        if c4==10:
+            four_stars+=1
+            print('4*',i,(four_stars,five_stars),(f'c4 {c4} c5 {c5}'))
+            c4=0
+    return four_stars,five_stars
+print(pull(191))
